c8e8407409f57a5218240a72fcbde057da8921e5
Alexander <Sakeran@gmail.com>
2021-04-12 15:08:34 -0400

    Style Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,15 @@
 import "./Footer.css";
 
-const Footer = () => <footer>FOOTER</footer>;
+const Footer = () => (
+  <footer>
+    <p>
+      Created by Alexander Morse, as part of his{" "}
+      <a href="https://10in30.alexander-morse.com">
+        Spring Web Development Challenge
+      </a>
+      .
+    </p>
+  </footer>
+);
+
 export default Footer;
